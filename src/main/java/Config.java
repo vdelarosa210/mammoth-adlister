5bef6a108eb1ce561944a2a4ad3800f1f2245cac
violet <vdelarosa210@gmail.com>
2017-06-09 13:53:49 -0500

    Created a class named Config.java that will hold our database configuration. Added 3 private strings, url, user, and password, as well as getters for those properties.

---
+++ src/main/java/Config.java
@@ -0,0 +1,22 @@
+/**
+ * Created by violet on 6/9/17.
+ */
+public class Config {
+    private String url;
+    private String user;
+    private String password;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+
+}
